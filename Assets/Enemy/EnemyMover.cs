9527619027b1fcb21917165a1b378ff2c3b9b96f
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-01-31 07:32:01 +0200

    Finding the Path

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -9,9 +9,28 @@ public class EnemyMover : MonoBehaviour
     
     void Start()
     {
+        FindPath();
+        ReturnToStart();
         StartCoroutine(FollowPath());
     }
 
+    void FindPath()
+    {
+        path.Clear();
+        
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Path");
+
+        foreach(GameObject waypoint in waypoints)
+        {
+            path.Add(waypoint.GetComponent<Waypoint>());
+        }
+    }
+    
+    void ReturnToStart()
+    {
+        transform.position = path[0].transform.position;
+    }
+    
     IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
@@ -29,6 +48,8 @@ public class EnemyMover : MonoBehaviour
             }
             
         }
+
+        Destroy(gameObject);
     }
 
 }
